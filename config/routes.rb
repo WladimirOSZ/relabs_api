45d8fd09be7a5bedb814e6a21cbd2e139367f61b
Wladimir Oliveira Souza <wladimir.souza@rebase.com.br>
2024-03-05 09:38:57 -0300

    Adding auth with devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'devise_token_auth'
+
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
